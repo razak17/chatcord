a1979a0b0279dd5cf999afceb9e5645c69ff8587
Abdul Razak <moabdulrazak17@gmail.com>
2020-02-21 17:23:33 +0000

    Fixing username validation for edit profile

--- app/forms.py
+++ app/forms.py
@@ -15,13 +15,32 @@ class EditProfileForm(FlaskForm):
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
 
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
     def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
-        if user is not None:
-            raise ValidationError("Username has already been taken!")
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=username.data).first()
+            if user is not None:
+                raise ValidationError("Username has already been taken!")
 
 
 class RegistrationForm(FlaskForm):
+    """Registration
+    
+    Parameters
+    ----------
+    FlaskForm : Form
+        User registration form
+    
+    Raises
+    ------
+    ValidationError
+        Username is required
+    ValidationError
+        Passwords must match
+    """
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
