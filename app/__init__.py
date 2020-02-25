68ffb0968a40a7a49d616c9389c517bd5b05a6be
Abdul Razak <moabdulrazak17@gmail.com>
2020-02-25 22:29:43 +0000

    basic styling

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import logging
 import os
@@ -11,13 +12,15 @@ import os
 app = Flask(__name__)
 
 app.config.from_object(Config)
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
 login = LoginManager(app)
 login.login_view = 'login'
 
-
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 
 # Config of errors to Email
